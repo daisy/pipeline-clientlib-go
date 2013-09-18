93976b2866464b5942c0e124932384166525bc18
capitancambio <capitan.cambio@gmail.com>
2013-09-19 00:07:45 +0100

    Add multipart encoder v1

--- encoders.go
+++ encoders.go
@@ -1,12 +1,62 @@
 package pipeline
 
 import (
+	"encoding/xml"
 	"errors"
 	"github.com/capitancambio/restclient"
 	"io"
 	"io/ioutil"
+	"mime/multipart"
 )
 
+type MultipartData struct {
+	request JobRequest
+	data    RawData
+}
+
+type MultipartEncoder struct {
+	writer   io.Writer
+	boundary string
+}
+
+func NewMultipartEncoder(w io.Writer) *MultipartEncoder {
+	return &MultipartEncoder{w, ""}
+}
+func (me MultipartEncoder) Encode(v interface{}) error {
+	//get the fields
+	var mpData *MultipartData
+	switch v.(type) {
+	case *MultipartData:
+		mpData = v.(*MultipartData)
+	default:
+		return errors.New("MultipartEncoder only admits MultipartData")
+	}
+
+	w := multipart.NewWriter(me.writer)
+	if me.boundary != "" {
+		w.SetBoundary(me.boundary)
+	}
+	reqWriter, err := w.CreateFormField("job-request")
+	if err != nil {
+		return err
+	}
+	err = xml.NewEncoder(reqWriter).Encode(mpData.request)
+	if err != nil {
+		return err
+	}
+	dataWriter, err := w.CreateFormField("job-data")
+	if err != nil {
+		return err
+	}
+	err = NewRawDataEncoder(dataWriter).Encode(mpData.data)
+	if err != nil {
+		return err
+	}
+	if err := w.Close(); err != nil {
+		return err
+	}
+	return nil
+}
 
 //Raw data struct defines a simple structure to
 //store bytes
@@ -14,8 +64,14 @@ type RawData struct {
 	Data *[]byte //Data
 }
 
+//Sets the bytes to the raw data structure
 func (r *RawData) SetBytes(b []byte) {
 	r.Data = &b
+}
+
+//Gets the bytes from the RawData structure
+func (r RawData) Bytes() []byte {
+	return *r.Data
 }
 
 type ToBytes interface {
@@ -29,6 +85,11 @@ type FromBytes interface {
 //RawDataDecoder allows to decode raw data into a RawData structure
 type RawDataDecoder struct {
 	reader io.Reader
+}
+
+//RawDataDecoder allows to decode raw data into a RawData structure
+type RawDataEncoder struct {
+	writer io.Writer
 }
 
 //Decodes the data into a raw data struct
@@ -46,7 +107,106 @@ func (d RawDataDecoder) Decode(v interface{}) error {
 	return nil
 }
 
+//Decodes the data into a raw data struct
+func (d RawDataEncoder) Encode(v interface{}) error {
+	var data []byte
+	switch v.(type) {
+	case ToBytes:
+		data = (v.(ToBytes)).Bytes()
+	default:
+		return errors.New("RawDataDecoder only admits ToBytes interface")
+	}
+	_, err := d.writer.Write(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 //Builds a RawDataDecoder
 func NewRawDataDecoder(r io.Reader) restclient.Decoder {
 	return RawDataDecoder{r}
 }
+
+//Builds a RawDataDecoder
+func NewRawDataEncoder(w io.Writer) restclient.Encoder {
+	return RawDataEncoder{w}
+}
+
+//TODO: the following could be a hell of a library if finished
+//const (
+//XML  = "application/xml"
+//DATA = "pplication/octet-stream"
+//)
+
+//type EncoderSupplier func(io.Writer) restclient.Encoder
+
+////registered suppliers
+//var suppliers map[string]EncoderSupplier = map[string]EncoderSupplier{
+//XML:  func(w io.Writer) restclient.Encoder { return xml.NewEncoder(w) },
+//DATA: NewRawDataEncoder,
+//}
+
+////type might change to sth more appropriate
+////func Register(mimetype string, s EncoderSupplier) {
+////suppliers[mimetype] = s
+////}
+
+////Multipart encoder encodes multypart messages
+//type MultipartEncoder struct {
+//w io.Writer
+//}
+
+//func (me MultipartEncoder) Encode(v interface{}) error {
+////get the fields
+//_, err := getFieldsMimeTypes(v)
+//if err != nil {
+//return err
+//}
+////get the encoder for each field
+
+////write
+//return nil
+//}
+
+//type multipartEntry struct {
+//name     string
+//mimetype string
+//}
+
+//type MPartEncoderError struct {
+//Type reflect.Type
+//}
+
+//func (e MPartEncoderError) Error() string {
+//return "multipart: could not encode type " + e.Type.String()
+//}
+
+//func getFieldsMimeTypes(i interface{}) (fields map[reflect.StructField]multipartEntry, err error) {
+//t := reflect.TypeOf(i)
+//if t == nil {
+//return fields, MPartEncoderError{t}
+//}
+////check that t is a pointer and it's accessible
+//if t.Kind() == reflect.Ptr {
+////get the referenced type
+//t = reflect.Indirect(reflect.ValueOf(i)).Type()
+//}
+//if t.Kind() != reflect.Struct {
+//return fields, MPartEncoderError{t}
+//}
+//fields = make(map[reflect.StructField]multipartEntry)
+////get the fields
+//for i := 0; i < t.NumField(); i++ {
+//if tag := t.Field(i).Tag; tag != "" {
+//mimetype := tag.Get("mimetype")
+//name := tag.Get("name")
+//if name == "" && mimetype == "" {
+//break
+//} else if name != "" && mimetype != "" {
+//fields[t.Field(i)] = multipartEntry{name, mimetype}
+//}
+//}
+//}
+//return
+//}
--- encoders_test.go
+++ encoders_test.go
@@ -2,6 +2,8 @@ package pipeline
 
 import (
 	"bytes"
+	"encoding/xml"
+	"mime/multipart"
 	"testing"
 )
 
@@ -31,3 +33,167 @@ func TestRawDataDecoderWrongType(t *testing.T) {
 	}
 
 }
+
+func TestRawDataEncoder(t *testing.T) {
+	msg := []byte("heyhey")
+	st := RawData{Data: &msg}
+	buf := bytes.NewBufferString("")
+	encoder := NewRawDataEncoder(buf)
+	err := encoder.Encode(&st)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	res := string(buf.Bytes())
+	if string(msg) != res {
+		t.Errorf("Wrong %v\n\tExpected: %v\n\tResult: %v", "msg", string(msg), res)
+	}
+}
+
+func TestRawDataEncoderErr(t *testing.T) {
+	st := "heyy"
+	buf := bytes.NewBufferString("")
+	encoder := NewRawDataEncoder(buf)
+	err := encoder.Encode(&st)
+	if err == nil {
+		println(err.Error())
+		t.Error("Expected error not thrown")
+	}
+}
+
+func TestMultipartEncodingErr(t *testing.T) {
+	buf := bytes.NewBufferString("")
+	err := NewMultipartEncoder(buf).Encode("hola")
+	if err == nil {
+		t.Error("Expected error not thrown")
+	}
+}
+
+func TestMultipartEncoding(t *testing.T) {
+	boundary := "inevergonnagiveyouup"
+	msg := []byte("hey yo")
+	rData := RawData{Data: &msg}
+	scrId := "test"
+	jobReq := JobRequest{
+		Script: Script{Id: scrId},
+	}
+
+	st := MultipartData{
+		request: jobReq,
+		data:    rData,
+	}
+	buf := bytes.NewBufferString("")
+	enc := NewMultipartEncoder(buf)
+	enc.boundary = boundary
+	err := enc.Encode(&st)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	r := multipart.NewReader(buf, boundary)
+	form, err := r.ReadForm(1024)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+
+	res := form.Value["job-data"][0]
+	if string(msg) != res {
+		t.Errorf("Wrong %v\n\tExpected: %v\n\tResult: %v", "string(msg) ", string(msg), res)
+	}
+	buf = bytes.NewBufferString(form.Value["job-request"][0])
+	resJob := JobRequest{}
+	err = xml.NewDecoder(buf).Decode(&resJob)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+
+	if scrId != resJob.Script.Id {
+		t.Errorf("Wrong %v\n\tExpected: %v\n\tResult: %v", "script id", scrId, resJob.Script.Id)
+	}
+}
+
+//func TestGetFieldsMimeTypesErrors(t *testing.T) {
+////not a struct
+//_, err := getFieldsMimeTypes("hola")
+//if err == nil {
+//t.Errorf("Expected error not thrown")
+//}
+//var st interface{} = nil
+//_, err = getFieldsMimeTypes(st)
+//if err == nil {
+//t.Errorf("Expected error not thrown")
+//}
+//}
+
+//type TestMP1 struct {
+//part1 interface{} `mimetype:"xml" name:"part1"`
+//part2 interface{} `mimetype:"json" name:"part2"`
+//}
+
+//func TestGetFieldsMimeTypes(t *testing.T) {
+//partType1 := "xml"
+//partType2 := "json"
+//partName1 := "part1"
+//partName2 := "part2"
+//mp := TestMP1{part1: "hola", part2: "hey"}
+////not a struct
+//mtypes, err := getFieldsMimeTypes(mp)
+//if err != nil {
+//t.Errorf("Unexpected error %v", err)
+//}
+//if len(mtypes) != 2 {
+//t.Errorf("Unexpected map lenght %v", len(mtypes))
+//}
+//resType, ok := mtypes[partName1]
+
+//if !ok {
+//t.Errorf("part1 was not stored")
+//}
+
+//if partType1 != resType {
+//t.Errorf("Wrong %v\n\tExpected: %v\n\tResult: %v", "partType ", partType1, resType)
+//}
+
+//resType, ok = mtypes[partName2]
+
+//if !ok {
+//t.Errorf("part2 was not stored")
+//}
+
+//if partType2 != resType {
+//t.Errorf("Wrong %v\n\tExpected: %v\n\tResult: %v", "partType ", partType2, resType)
+//}
+//}
+
+//func TestGetFieldsMimeTypesPoiter(t *testing.T) {
+//partType1 := "xml"
+//partType2 := "json"
+//partName1 := "part1"
+//partName2 := "part2"
+//mp := TestMP1{part1: "hola", part2: "hey"}
+////not a struct
+//mtypes, err := getFieldsMimeTypes(&mp)
+//if err != nil {
+//t.Errorf("Unexpected error %v", err)
+//}
+//if len(mtypes) != 2 {
+//t.Errorf("Unexpected map lenght %v", len(mtypes))
+//}
+//resType, ok := mtypes[partName1]
+
+//if !ok {
+//t.Errorf("part1 was not stored")
+//}
+
+//if partType1 != resType {
+//t.Errorf("Wrong %v\n\tExpected: %v\n\tResult: %v", "partType ", partType1, resType)
+//}
+
+//resType, ok = mtypes[partName2]
+
+//if !ok {
+//t.Errorf("part1 was not stored")
+//}
+
+//if partType2 != resType {
+//t.Errorf("Wrong %v\n\tExpected: %v\n\tResult: %v", "partType ", partType2, resType)
+//}
+//}
